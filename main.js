9bb25a0eb130037fa6d7120879170fba2d23b7bb
AmilaE <ireeshika@gmail.com>
2019-03-18 12:03:27 +0530

    console log variable_declaration

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-// Block everything and shows the notification
-alert('Hello World');
+// // Block everything and shows the notification
+// alert('Hello World');
 
-//output a message to  the console log
-console.log('Hello World');
+// //output a message to  the console log
+// console.log('Hello World');
+// console.warn('This is a warning');
+// console.error('This is an error');
+
+//Variables
+//Let
+let score;
+score = 10;
+score = 10+6;
+console.log(score);
+
+//Const: Need to be initialized when declaring, can't re assign, but safe and robust
+const score_c = 10;
+console.log(score_c);
