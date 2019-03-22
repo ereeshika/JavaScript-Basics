f845679c83b10ab712b29f797302ee2c25f7c7dd
AmilaE <ireeshika@gmail.com>
2019-03-22 06:17:34 +0530

    Data Types

--- main.js
+++ main.js
@@ -6,13 +6,30 @@
 // console.warn('This is a warning');
 // console.error('This is an error');
 
-//Variables
-//Let
-let score;
-score = 10;
-score = 10+6;
-console.log(score);
+// //Variables
+// //Let
+// let score;
+// score = 10;
+// score = 10+6;
+// console.log(score);
+//
+// //Const: Need to be initialized when declaring, can't re assign, but safe and robust
+// const score_c = 10;
+// console.log(score_c);
 
-//Const: Need to be initialized when declaring, can't re assign, but safe and robust
-const score_c = 10;
-console.log(score_c);
+//Data Types
+const name = 'John';
+const age = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined; //undefined
+let z; //undefined
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof rating);
+console.log(typeof isCool);
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
